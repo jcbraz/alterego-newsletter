bd937d1655da04d9298945abc18845fe226c0dbe
jcbraz <josecbraz@outlook.com>
2023-10-07 18:00:04 +0200

    Add accordion

---
+++ src/components/ui/requirements-according.tsx
@@ -0,0 +1,28 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const RequirementsAccording = () => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger className="text-colorBlack italic font-semibold lg:text-lg text-base">
+          Subscription Requirements
+        </AccordionTrigger>
+        <AccordionContent>
+          <h4 className="text-colorBlack italic font-normal lg:text-base text-sm">You should not subscribe to this newsletter if</h4>
+          <ul className="list-disc font-medium space-y-4 pt-4">
+            <li>- Reading a 2 minutes piece is too much for you. Maybe thoose 4 Tik Tok videos take the same time and the dopamine is much easier. As they all say, <span className="text-colorOrange font-semibold">Time is money.</span></li>
+            <li>- You think is not worth it to receive one more <span>annoying</span> email everyday. If you've already subscribe and share this opinion, I really encourage you to unsubscribe but remember, there's probably more to win than to loose. <span className="text-colorOrange font-semibold">Your choices determine you're destiny.</span></li>
+            <li>- Critical thinking, new ideas and overall self-improvement are not interesting to you.</li>
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default RequirementsAccording;
